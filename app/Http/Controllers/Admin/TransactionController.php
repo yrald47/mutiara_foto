14a59fdd4ee0ed92559269c4e674f79347036dae
RALD <yr.alditya@gmail.com>
2022-11-21 11:23:22 +0700

    apus komen TransactionController

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -11,16 +11,9 @@ class TransactionController extends Controller
 {
     public function index()
     {
-        // $booking = Booking::all();
         $booking = Booking::join('users', 'users.id', '=', 'bookings.id_member')
                             ->join('packages', 'packages.kode_paket', '=', 'bookings.kode_paket')
                             ->get(['bookings.*', 'packages.*', 'users.*']);
-        // dd($booking);
         return view('admin.transactions.index', compact('booking'));
-        // $users = Booking::join('contacts', 'users.id', '=', 'contacts.user_id')
-        //     ->join('orders', 'users.id', '=', 'orders.user_id')
-        //     ->select('users.*', 'contacts.phone', 'orders.price')
-        //     ->get();
-
     }
 }
